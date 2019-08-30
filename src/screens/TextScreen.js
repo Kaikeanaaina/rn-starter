7c126a2f300e1d4601bcb074c69e27bc28f2bbc8
KainoaKeanaaina <kaikeanaaina@gmail.com>
2019-08-30 13:49:05 -1000

    Box Object Model

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
+    //everytime we have a text input, we will have a state and a onChangeText
+    const [password, setPassword] = useState('')
     return (
         <View>
+            <Text> Enter Password:</Text>
             <TextInput 
                 style={styles.input} 
                 // were use autoCapitalize and autoCorrect, so the keyboard doens't automatically correct the email
                 autoCapitalize="none"
                 autoCorrect={false}
+                value={password}
+                onChangeText={(newValue) => setPassword(newValue) }
             />
+            {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
         </View>
     )
 }
